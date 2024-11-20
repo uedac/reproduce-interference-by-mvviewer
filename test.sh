cb8e7d96eb2ab24e1719485ef6540127b64c2255
uedac <ueda@chitose-robotics.com>
2024-11-20 16:38:15 +0900

    test

--- test.sh
+++ test.sh
@@ -2,19 +2,14 @@
 
 set -ex
 
-cut -d: -f1 /etc/passwd
 ls /opt/HuarayTech/MVviewer/include/ -la
 ls /opt/HuarayTech/MVviewer/lib/ -la
 ls /opt/HuarayTech/MVviewer/bin/ -la
-
-sudo chmod -R g-r /opt/HuarayTech/MVviewer/include/*
-whoami
-groups runner
 
 directory_path_of_this_script=$(dirname $(readlink -f "$0"))
 rm $directory_path_of_this_script/build -rf
 mkdir $directory_path_of_this_script/build
 cd $directory_path_of_this_script/build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-make
+groups | make
 $directory_path_of_this_script/build/main
