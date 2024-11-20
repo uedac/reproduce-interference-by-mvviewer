0e575dcc71c568d05875f84bb93d58e5fd884b29
uedac <ueda@chitose-robotics.com>
2024-11-20 14:55:04 +0900

    test

--- test.sh
+++ test.sh
@@ -2,8 +2,7 @@
 
 set -ex
 
-whoami
-getent group root
+sudo chmod -R -r /opt/HuarayTech/MVviewer/include/*
 directory_path_of_this_script=$(dirname $(readlink -f "$0"))
 rm $directory_path_of_this_script/build -rf
 mkdir $directory_path_of_this_script/build
