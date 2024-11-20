a0e1efe798e0206fc7ca7d7cce0db17712b2971c
uedac <ueda@chitose-robotics.com>
2024-11-20 14:22:26 +0900

    パーミッション周りの確認

--- library_installers/mvviewer_installer/install_mvviewer.sh
+++ library_installers/mvviewer_installer/install_mvviewer.sh
@@ -6,3 +6,8 @@ installer_file_name=MVviewer_Ver2.4.2.9_Linux_x86_Build20240923.run
 wget -P /tmp https://github.com/sakurai-ryuhei/reproduce-interference-between-librealsense2-and-mvviewer/releases/download/v0.0.0/$installer_file_name
 chmod +x /tmp/$installer_file_name
 yes yes | sudo /tmp/$installer_file_name --nox11
+
+whoami
+ls /opt/HuarayTech/MVviewer/include/ -la
+ls /opt/HuarayTech/MVviewer/lib/ -la
+ls /opt/HuarayTech/MVviewer/bin/ -la
